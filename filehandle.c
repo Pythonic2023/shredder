4bb3b76f229cf8348a7995f303dda888776cc47c
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-03-14 07:21:33 -0400

    filehandle.c now overwrites file with /dev/zero

--- filehandle.c
+++ filehandle.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -24,7 +25,7 @@ int file_type(const char *string_buffer, char *file){
 void over_write(const char *string_buffer, char *file){
 	char buffer[2]; /* Buffer to hold answer of y(es)/n(o) */
 	printf("### You are being asked to overwrite a file, please proceed with caution. ###\n\n### Provide y OR n ###\n\n");
-	printf("Overwrite %s at %s?\n", file, string_buffer);
+	printf("Overwrite %s at %s? ", file, string_buffer);
 	
 	if(fgets(buffer, 2, stdin) == NULL){
 		printf("error reading");
@@ -33,7 +34,20 @@ void over_write(const char *string_buffer, char *file){
 		printf("[EXITING]\n");
 	}
 	else if(strcmp(buffer, "y") == 0){
-		printf("[OVERWRITING...]\n");	
+		int count = 0;
+		FILE *file_stream = fopen(string_buffer, "r");
+		fseek(file_stream, 0, SEEK_END);
+		long size = ftell(file_stream);
+		fseek(file_stream, 0, SEEK_SET);
+		char command_buffer[1024];
+		snprintf(command_buffer, sizeof(command_buffer), "dd if=/dev/zero of=%s bs=1 count=%ld", string_buffer, size);
+		
+		if(system(command_buffer) == -1){
+			printf("Failed to execute %s\n", command_buffer);
+		}else{
+			printf("\n[OVERWRITTEN]\n");
+		}
+			
 		exit(0);
 	}
 	else{
